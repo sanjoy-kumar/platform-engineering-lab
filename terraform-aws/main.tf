89e9666f639755f4b2f040928e50fe7e1084212c
Sanjoy Kumar Das <sanjoy.kumar.backup@gmail.com>
2026-08-11 00:09:35 -0400

    Create a security group and add with the VM in the AWS using terraform

--- terraform-aws/main.tf
+++ terraform-aws/main.tf
@@ -2,6 +2,7 @@
 resource "aws_instance" "web_server" {
     ami = "ami-0e5497a77ef21b5ac"
     instance_type = var.instance_type
+    vpc_security_group_ids = [ aws_security_group.web_sg.id ]
 
     tags  = {
         Name = "web-server"
@@ -9,3 +10,44 @@ resource "aws_instance" "web_server" {
 
 }
 
+
+
+resource "aws_security_group" "web_sg" {
+    name = "web-sg"
+    description = "Allow SSH, HTTP and HTTPS traffic."
+
+    # SSH 
+    ingress {
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = [ "0.0.0.0/0" ]
+    }
+
+    # HTTP
+
+    ingress {
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = [ "0.0.0.0/0" ]
+    }
+
+    # HTTPS
+
+    ingress {
+        from_port = 443
+        to_port = 443
+        protocol = "tcp"
+        cidr_blocks = [ "0.0.0.0/0" ]
+    }
+
+
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = [ "0.0.0.0/0" ]
+    }
+
+}
